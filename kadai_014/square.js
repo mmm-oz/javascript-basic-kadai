d3273a32fabe8abdfa9fbaf54aa709c6a793761c
mmm-oz <147613964+mmm-oz@users.noreply.github.com>
2023-11-23 11:00:10 +0900

    kadai_014を追加

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -5,3 +5,11 @@ const calc = (double) => {
 
 // コンソールに表示
 console.log(calc(10));
+
+// 上記の戻り値を引数だけで出せないか
+
+// const calc2 = (double2) => {
+//     console.log(double2*double2);
+// }
+
+// calc2(10);
